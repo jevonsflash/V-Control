d0b6c41ee9fb2d3e48a7d468d33ee884b194e22a
jevonslin <jevonsflash@qq.com>
2025-03-28 18:25:17 +0800

    fix: System.NullReferenceException when click uploader items

--- src/Controls/VUploader.xaml.cs
+++ src/Controls/VUploader.xaml.cs
@@ -141,7 +141,7 @@ public partial class VUploader : ContentView
 
     private void TapGestureRecognizer_Tapped_1(object sender, TappedEventArgs e)
     {
-        var CommandParameter = (sender as VUploader).BindingContext;
+        var CommandParameter = (sender as BindableObject).BindingContext;
         this.Command?.Execute(CommandParameter);
     }
 
@@ -151,7 +151,7 @@ public partial class VUploader : ContentView
         {
             return;
         }
-        var removeCommandParameter = (sender as VUploader).BindingContext;
+        var removeCommandParameter = (sender as BindableObject).BindingContext;
         RemoveCommand?.Execute(removeCommandParameter);
     }
 }
