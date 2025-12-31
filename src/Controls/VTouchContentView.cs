dfa562d9532c5469bd0be925965f5b6dd01f2eb4
jevonslin <jevonsflash@qq.com>
2025-12-31 09:21:46 +0800

    feat: add touch content veiw exception handling

--- src/Controls/VTouchContentView.cs
+++ src/Controls/VTouchContentView.cs
@@ -15,46 +15,75 @@
         private void TouchContentView_HandlerChanged(object sender, EventArgs e)
         {
             var handler = Handler;
-            if (handler != null)
-            {
-#if ANDROID
-                touchRecognizer = new TouchRecognizer_Android(
-                    handler.PlatformView as Android.Views.View
-                );
-                touchRecognizer.OnTouchActionInvoked += TouchRecognizer_OnTouchActionInvoked;
+            if (handler == null)
+                return;
 
+#if ANDROID
+            var platformView = handler.PlatformView as Android.Views.View;
+            if (platformView == null)
+                return;
+
+            // cleanup any existing recognizer
+            if (touchRecognizer != null)
+            {
+                try { touchRecognizer.OnTouchActionInvoked -= TouchRecognizer_OnTouchActionInvoked; } catch { }
+                try { touchRecognizer.Dispose(); } catch { }
+                touchRecognizer = null;
+            }
+
+            touchRecognizer = new TouchRecognizer_Android(platformView);
+            touchRecognizer.OnTouchActionInvoked += TouchRecognizer_OnTouchActionInvoked;
 #endif
 
 #if IOS|| MACCATALYST
-                touchRecognizer = new TouchRecognizer_iOS(handler.PlatformView as UIKit.UIView);
-                touchRecognizer.OnTouchActionInvoked += TouchRecognizer_OnTouchActionInvoked;
+            var uiView = handler.PlatformView as UIKit.UIView;
+            if (uiView == null)
+                return;
 
-                (handler.PlatformView as UIKit.UIView).UserInteractionEnabled = true;
-                (handler.PlatformView as UIKit.UIView).AddGestureRecognizer(
-                    touchRecognizer as TouchRecognizer_iOS
-                );
-#endif
+            if (touchRecognizer != null)
+            {
+                try { touchRecognizer.OnTouchActionInvoked -= TouchRecognizer_OnTouchActionInvoked; } catch { }
+                try { touchRecognizer.Dispose(); } catch { }
+                touchRecognizer = null;
             }
+
+            touchRecognizer = new TouchRecognizer_iOS(uiView);
+            touchRecognizer.OnTouchActionInvoked += TouchRecognizer_OnTouchActionInvoked;
+
+            uiView.UserInteractionEnabled = true;
+            try { uiView.AddGestureRecognizer(touchRecognizer as TouchRecognizer_iOS); } catch { }
+#endif
         }
 
         private void TouchContentView_HandlerChanging(object sender, HandlerChangingEventArgs e)
         {
             if (e.OldHandler != null)
             {
-                var handler = e.OldHandler;
-
 #if ANDROID
-                touchRecognizer.OnTouchActionInvoked -= TouchRecognizer_OnTouchActionInvoked;
-
+                if (touchRecognizer != null)
+                {
+                    try { touchRecognizer.OnTouchActionInvoked -= TouchRecognizer_OnTouchActionInvoked; } catch { }
+                    try { touchRecognizer.Dispose(); } catch { }
+                    touchRecognizer = null;
+                }
 #endif
 
 #if IOS|| MACCATALYST
-                touchRecognizer.OnTouchActionInvoked -= TouchRecognizer_OnTouchActionInvoked;
+                if (touchRecognizer != null)
+                {
+                    try { touchRecognizer.OnTouchActionInvoked -= TouchRecognizer_OnTouchActionInvoked; } catch { }
 
-                (handler.PlatformView as UIKit.UIView).UserInteractionEnabled = false;
-                (handler.PlatformView as UIKit.UIView).RemoveGestureRecognizer(
-                    touchRecognizer as TouchRecognizer_iOS
-                );
+                    var uiView = e.OldHandler.PlatformView as UIKit.UIView;
+                    var gestureRecognizer = touchRecognizer as TouchRecognizer_iOS;
+                    if (uiView != null && gestureRecognizer != null)
+                    {
+                        try { uiView.UserInteractionEnabled = false; } catch { }
+                        try { uiView.RemoveGestureRecognizer(gestureRecognizer); } catch { }
+                    }
+
+                    try { touchRecognizer.Dispose(); } catch { }
+                    touchRecognizer = null;
+                }
 #endif
             }
         }
